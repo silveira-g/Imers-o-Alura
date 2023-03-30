efef9a8eb4b2f6d7d395357774627b81874b7b0b
silveira-g <silveira.alves.guilherme@gmail.com>
2023-03-30 20:56:50 -0300

    Criacao da classe ClienteHttp

--- imsersão-stickers/src/App.java
+++ imsersão-stickers/src/App.java
@@ -1,64 +1,40 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
-                // fazer uma conexão HTTP e buscar os top 250 filmes
-                String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
-                URI endereco = URI.create(url);
-                var client = HttpClient.newHttpClient();
-                HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
-                HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-                String body = response.body();
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = 
+        "https://api.nasa.gov/planetary/apod?api_key=9FgV5L3mhRNZW5bVNThHMKhhtEAKyoJLOs5zKkA3&start_date=2022-10-13&end_date=2022-10-15";
+        
+        var http = new ClienteHttp();
 
-                // pegar somente os dados que interessam (titulo, poster, classificação)
-                var parser = new Parser();
-                List<Map<String, String>> listaDeSeries = parser.parse(body);
-                // exibir e manupular os dados
-                var geradora = new GeradorDeFigurinha();
-                var diretorio = new File("figurinhas/");
-                diretorio.mkdir();
-                String texto = "Nem perca tempo";
-                for (Map<String,String> serie : listaDeSeries) {
-                    String urlImagem = serie.get("image");
-                    String titulo = serie.get("title");
-                    
-                    InputStream inputStream = new URL(urlImagem).openStream();
+        String json = http.buscaDados(url);
 
-                    String nomeArquivo = "figurinhas/" + titulo + ".png";
+        // pegar somente os dados que interessam (titulo, poster, classificação)
+        var parser = new Parser();
+        List<Map<String, String>> listaDeConteudos = parser.parse(json);
 
-                    
-                    String nota = serie.get("imDbRating");
-                    double notaD = Double.parseDouble(nota);
-                    InputStream meme = new FileInputStream(new File("sobreposicao/perdadetempo.png"));
-                    if (notaD>=8.5){ 
-                        texto="INCRIVEL!!!";
-                        meme = new FileInputStream(new File("sobreposicao/incrivel.png"));
-                    } else if (notaD>=6){
-                        texto="Série ok";
-                        meme = new FileInputStream(new File("sobreposicao/ok.png"));
-                    } 
+        // exibir e manupular os dados
+        var geradora = new GeradorDeFigurinha();
+        var diretorio = new File("figurinhas/");
+        diretorio.mkdir();
+        String texto = "Show";
+        for (Map<String, String> conteudo : listaDeConteudos) {
+            String urlImagem = conteudo.get("url");
+            String titulo = conteudo.get("title");
 
-                    
-                    
-                    
+            InputStream inputStream = new URL(urlImagem).openStream();
 
+            String nomeArquivo = "figurinhas/" + titulo + ".png";
 
-                    geradora.cria(inputStream, nomeArquivo, texto, meme);
+            geradora.cria(inputStream, nomeArquivo, texto);
 
-                    System.out.println("\u001b[1m \u001b[44m Titulo: \u001b[m \u001b[m" + serie.get("title"));
-                    System.out.println(notaD);
-                    System.out.println();
-                }
+            System.out.println("\u001b[1m \u001b[44m Titulo: \u001b[m \u001b[m" + conteudo.get("title"));
+            System.out.println();
+        }
     }
 }
