a716ab889e18574e73bb6e8f250c80a168721718
silveira-g <silveira.alves.guilherme@gmail.com>
2023-03-29 23:22:18 -0300

    Configura o codigo para escrever na imagem

--- imsersão-stickers/src/GeradorDeFigurinha.java
+++ imsersão-stickers/src/GeradorDeFigurinha.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -21,10 +23,17 @@ public class GeradorDeFigurinha {
         //copiar imagem original pra nova (em memória)
         Graphics2D grafico = (Graphics2D) novaImgagem.getGraphics();
         grafico.drawImage(imagemOriginal, 0, 0, null);
+
+        //configurar a fonte
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        grafico.setColor(Color.MAGENTA);
+        grafico.setFont(fonte);
+
         //escrever uma frase na nova imagem
-        ImageIO.write(novaImgagem, "png", new File("saida/figurinha.png"));
+        grafico.drawString("SHOW", (novaLargura/2)-100, novaAltura-100);     
 
         //escrever a nova imagem em um arquivo
+        ImageIO.write(novaImgagem, "png", new File("saida/figurinha.png"));
     }
     public static void main(String[] args) throws Exception {
         var geradora = new GeradorDeFigurinha();
