cbcbd54797a8376fc823d063c07491676fd92bf9
silveira-g <silveira.alves.guilherme@gmail.com>
2023-03-30 22:37:36 -0300

    Implementacao da interface

--- imsersão-stickers/src/ExtratorDeConteudoDaImdb.java
+++ imsersão-stickers/src/ExtratorDeConteudoDaImdb.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class ExtratorDeConteudoDaImdb {
-    List<Conteudo> extraiConteudos(String json){ 
+public class ExtratorDeConteudoDaImdb implements ExtratorDeConteudo {
+    public List<Conteudo> extraiConteudos(String json){ 
         var parser = new Parser();
         List<Map<String, String>> listaDeAtributos = parser.parse(json);
 
--- imsersão-stickers/src/ExtratorDeConteudoDaNasa.java
+++ imsersão-stickers/src/ExtratorDeConteudoDaNasa.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class ExtratorDeConteudoDaNasa {
-    List<Conteudo> extraiConteudos(String json){ 
+public class ExtratorDeConteudoDaNasa implements ExtratorDeConteudo {
+    public List<Conteudo> extraiConteudos(String json){ 
         var parser = new Parser();
         List<Map<String, String>> listaDeAtributos = parser.parse(json);
 
