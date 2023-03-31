aa0fe55d10f48c1a39f8c38499e5ad2e4c0f0bc4
silveira-g <silveira.alves.guilherme@gmail.com>
2023-03-30 22:32:22 -0300

    Inserindo url da imdb

--- imsersão-stickers/src/App.java
+++ imsersão-stickers/src/App.java
@@ -6,17 +6,20 @@ import java.util.List;
 public class App {
     public static void main(String[] args) throws Exception {
         // fazer uma conexão HTTP e buscar os top 250 filmes
-        String url = 
-        "https://api.nasa.gov/planetary/apod?api_key=9FgV5L3mhRNZW5bVNThHMKhhtEAKyoJLOs5zKkA3&start_date=2022-10-13&end_date=2022-10-15";
-        
+        // String url = 
+        // "https://api.nasa.gov/planetary/apod?api_key=9FgV5L3mhRNZW5bVNThHMKhhtEAKyoJLOs5zKkA3&start_date=2022-10-13&end_date=2022-10-15";
+        // var extrator = new ExtratorDeConteudoDaNasa();
+
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
+        var extrator = new ExtratorDeConteudoDaImdb();
         var http = new ClienteHttp();
 
         String json = http.buscaDados(url);
 
         // pegar somente os dados que interessam (titulo, poster, classificação)
 
-        var extrator = new ExtratorDeConteudoDaNasa();
-        List<Conteudo> conteudos =    extrator.extraiConteudos(json);
+        
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
 
         // exibir e manupular os dados
         var geradora = new GeradorDeFigurinha();
